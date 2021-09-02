6b6867d503a4cb989d8bd1d47d03ef24ccc1751e
AlexKuryanov <lexxus07@yandex.ru>
2021-09-02 20:47:19 +0300

    feat: solve three tests

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,24 @@
 class TicTacToe {
-    constructor() {
-
-    }
+  constructor(){
+  this.currentPlayer = 'x';
+  this.gameField = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ]
+  }
 
     getCurrentPlayerSymbol() {
-
+      return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+      if (this.gameField[rowIndex][columnIndex]) {
+        return;
+      } else {
+        this.gameField[rowIndex][columnIndex] = this.currentPlayer;
+        this.currentPlayer = this.currentPlayer == 'x' ? 'o' : 'x';
+      }
     }
 
     isFinished() {
@@ -20,15 +30,21 @@ class TicTacToe {
     }
 
     noMoreTurns() {
-
+      for (let i = 0; i < this.gameField.length; i++) {
+        for (let j = 0; j < this.gameField[i].length; j++) {
+          if (this.gameField[i][j] === null) {
+            return false;
+          }
+        }
+      }
+      return true;
     }
 
     isDraw() {
-
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+      return this.gameField[rowIndex][colIndex];
     }
 }
 
